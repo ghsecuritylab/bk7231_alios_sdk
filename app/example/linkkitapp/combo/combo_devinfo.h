390bb8d17114b3a0f418c847bb39990a25cf0ea2
junlong.zhou <junlong.zhou@bekencorp.com>
2019-05-17 18:02:56 +0800

    bk7231 alios2.1.0 sdk, base on internal git git@192.168.0.6:wifi/AliOS-Things-rel_2.1.0-bk7231-fullcode.git master branch 2019-05-17. version:  c9a1291a25e8499c63230f54e01efc57888c3488

---
+++ app/example/linkkitapp/combo/combo_devinfo.h
@@ -0,0 +1,11 @@
+#ifndef _DEV_INFO_H_
+#define _DEV_INFO_H_
+
+/* for demo only, subject to change to user's specific ones. */
+#define PRODUCT_ID     1762552
+#define DEVICE_SECRET  "QnYHmmhdeNKWigFhaE0HB8DnScPdV9km"
+#define DEVICE_NAME    "John_test"
+#define PRODUCT_KEY    "a1tvASemeJf"
+#define PRODUCT_SECRET "dqBnLjgaPhReAMr6"
+
+#endif
